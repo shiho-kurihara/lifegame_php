75338296b68db9415a7396668bc17c24fdc42178
shiho-kurihara <s_kurihara@codmon.co.jp>
2023-05-16 18:11:34 +0900

    fix: ifの中身を関数に切り出す

--- server/html/lifegame_logic.php
+++ server/html/lifegame_logic.php
@@ -4,110 +4,130 @@
  * - [x] $_GETを外から渡す
  * - [x] prevとgだけ渡すようにしてみる
  *      - [x] 2回目のテストを書く
- *      - [ ] 3回目のテストを書く
+ *      - [x] 3回目のテストを書く
+ * - [x] ifの中を関数に切り出す
  *
  * */
 function run_lifegame($prev,$g) {
     if (isset ($prev)) {
-        $b = [];
-        $lns = explode('9', $prev);
-        for ($i = 0, $len = count($lns); $i < $len; $i++) {
-            $ln = $lns[$i];
-            $r = [];
-            for ($j = 0, $len2 = mb_strlen($ln); $j < $len2; $j++) {
-                if ($ln[$j] == 0) {
-                    $r[] = '□';
-                } else {
-                    $r[] = '■';
-                }
-            }
-            $b[] = $r;
-        }
-        $bb = [];
-        for ($i = 0, $len = count($b); $i < $len; $i++) {
-            $r = [];
-            for ($j = 0, $len2 = count($b[$i]); $j < $len2; $j++) {
-                $r[] = '□';
-            }
-            $bb[] = $r;
-        }
-        for ($i = 0, $len = count($b); $i < $len; $i++) {
-            for ($j = 0, $len2 = count($b[$i]); $j < $len2; $j++) {
-                if ($b[$i][$j] === '□') {
-                    // 誕生の場合
-                    $count = 0;
-                    if ($i > 0 && $j > 0 && $b[$i - 1][$j - 1] === ("■")) $count += 1;
-                    if ($i > 0 && $b[$i - 1][$j] === "■") $count += 1;
-                    if ($i > 0 && $j < 4 && $b[$i - 1][$j + 1] === ("■")) $count += 1;
-                    if ($j > 0 && $b[$i][$j - 1] === ("■")) $count += 1;
-                    if ($j < 4 && $b[$i][$j + 1] === ("■")) $count += 1;
-                    if ($i < 4 && $j > 0 && $b[$i + 1][$j - 1] === ("■")) $count += 1;
-                    if ($i < 4 && $b[$i + 1][$j] === ("■")) $count += 1;
-                    if ($i < 4 && $j < 4 && $b[$i + 1][$j + 1] === ("■")) $count += 1;
-
-                    if ($count >= 3) {
-                        $bb[$i][$j] = "■";
-                    } else {
-                        $bb[$i][$j] = "□";
-                    }
-                }
-                if ($b[$i][$j] === ("■")) {
-                    // 生存・過疎・過密の場合
-                    $count = 0;
-
-                    if ($i > 0 && $b[$i - 1][$j] === ("■")) $count += 1;
-                    if ($j > 0 && $b[$i][$j - 1] === ("■")) $count += 1;
-                    if ($j < 4 && $b[$i][$j + 1] === ("■")) $count += 1;
-                    if ($i < 4 && $b[$i + 1][$j] === ("■")) $count += 1;
-
-                    if ($count == 2) {
-                        $bb[$i][$j] = $b[$i][$j];
-                    } else {
-                        $bb[$i][$j] = "□";
-                    }
-                }
-            }
-        }
-        $b = $bb;
-        $n = [];
-        for ($i = 0, $len = count($b); $i < $len; $i++) {
-            $nr = [];
-            for ($j = 0, $len2 = count($b[$i]); $j < $len2; $j++) {
-                if ($b[$i][$j] == '□') {
-                    $nr[] = '0';
-                } else {
-                    $nr[] = '1';
-                }
-            }
-            $n[] = implode('', $nr);
-        }
-        $s = implode('9', $n);
-        return [$g, $b, $s];
+        return getSecondAndAfterContents($prev, $g);
     } else {
-        $b = [];
-        for ($i = 0; $i < 5; $i++) {
-            for ($j = 0; $j < 5; $j++) {
-                $b[$i][$j] = "□";
+        return getFirstContents($g);
+    }
+}
+
+/**
+ * @param $g
+ * @return array
+ */
+function getFirstContents($g): array
+{
+    $b = [];
+    for ($i = 0; $i < 5; $i++) {
+        for ($j = 0; $j < 5; $j++) {
+            $b[$i][$j] = "□";
+        }
+    }
+    $b[2][1] = "■";
+    $b[2][2] = "■";
+    $b[2][3] = "■";
+    $n = [];
+    for ($i = 0; $i < 5; $i++) {
+        $r = $b[$i];
+        $nr = [];
+        for ($j = 0; $j < 5; $j++) {
+            if ($r[$j] == '□') {
+                $nr[] = '0';
+            } else {
+                $nr[] = '1';
             }
         }
-        $b[2][1] = "■";
-        $b[2][2] = "■";
-        $b[2][3] = "■";
-        $n = [];
-        for ($i = 0; $i < 5; $i++) {
-            $r = $b[$i];
-            $nr = [];
-            for ($j = 0; $j < 5; $j++) {
-                if ($r[$j] == '□') {
-                    $nr[] = '0';
+        $n[] = implode('', $nr);
+    }
+    $s = implode('9', $n);
+    $g = 1;
+    return [$g, $b, $s];
+}
+
+/**
+ * @param $prev
+ * @param $g
+ * @return array
+ */
+function getSecondAndAfterContents($prev, $g): array
+{
+    $b = [];
+    $lns = explode('9', $prev);
+    for ($i = 0, $len = count($lns); $i < $len; $i++) {
+        $ln = $lns[$i];
+        $r = [];
+        for ($j = 0, $len2 = mb_strlen($ln); $j < $len2; $j++) {
+            if ($ln[$j] == 0) {
+                $r[] = '□';
+            } else {
+                $r[] = '■';
+            }
+        }
+        $b[] = $r;
+    }
+    $bb = [];
+    for ($i = 0, $len = count($b); $i < $len; $i++) {
+        $r = [];
+        for ($j = 0, $len2 = count($b[$i]); $j < $len2; $j++) {
+            $r[] = '□';
+        }
+        $bb[] = $r;
+    }
+    for ($i = 0, $len = count($b); $i < $len; $i++) {
+        for ($j = 0, $len2 = count($b[$i]); $j < $len2; $j++) {
+            if ($b[$i][$j] === '□') {
+                // 誕生の場合
+                $count = 0;
+                if ($i > 0 && $j > 0 && $b[$i - 1][$j - 1] === ("■")) $count += 1;
+                if ($i > 0 && $b[$i - 1][$j] === "■") $count += 1;
+                if ($i > 0 && $j < 4 && $b[$i - 1][$j + 1] === ("■")) $count += 1;
+                if ($j > 0 && $b[$i][$j - 1] === ("■")) $count += 1;
+                if ($j < 4 && $b[$i][$j + 1] === ("■")) $count += 1;
+                if ($i < 4 && $j > 0 && $b[$i + 1][$j - 1] === ("■")) $count += 1;
+                if ($i < 4 && $b[$i + 1][$j] === ("■")) $count += 1;
+                if ($i < 4 && $j < 4 && $b[$i + 1][$j + 1] === ("■")) $count += 1;
+
+                if ($count >= 3) {
+                    $bb[$i][$j] = "■";
                 } else {
-                    $nr[] = '1';
+                    $bb[$i][$j] = "□";
                 }
             }
-            $n[] = implode('', $nr);
+            if ($b[$i][$j] === ("■")) {
+                // 生存・過疎・過密の場合
+                $count = 0;
+
+                if ($i > 0 && $b[$i - 1][$j] === ("■")) $count += 1;
+                if ($j > 0 && $b[$i][$j - 1] === ("■")) $count += 1;
+                if ($j < 4 && $b[$i][$j + 1] === ("■")) $count += 1;
+                if ($i < 4 && $b[$i + 1][$j] === ("■")) $count += 1;
+
+                if ($count == 2) {
+                    $bb[$i][$j] = $b[$i][$j];
+                } else {
+                    $bb[$i][$j] = "□";
+                }
+            }
         }
-        $s = implode('9', $n);
-        $g = 1;
-        return [$g, $b, $s];
     }
+    $b = $bb;
+    $n = [];
+    for ($i = 0, $len = count($b); $i < $len; $i++) {
+        $nr = [];
+        for ($j = 0, $len2 = count($b[$i]); $j < $len2; $j++) {
+            if ($b[$i][$j] == '□') {
+                $nr[] = '0';
+            } else {
+                $nr[] = '1';
+            }
+        }
+        $n[] = implode('', $nr);
+    }
+    $s = implode('9', $n);
+    return [$g, $b, $s];
 }
